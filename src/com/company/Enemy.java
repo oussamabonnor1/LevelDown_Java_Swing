37a3473ff5e56e6ed2cf35848555202b1b75a7a8
oussama <oussamabonnor@gmail.com>
2017-05-05 00:20:40 +0100

    Graphic phase 1 : enemies spawning done, positioning problem

---
+++ src/com/company/Enemy.java
@@ -0,0 +1,50 @@
+package com.company;
+
+/**
+ * Created by Oussama on 04/05/2017.
+ */
+public class Enemy {
+    private int width;
+    private int height;
+    private int posX;
+    private int posY;
+
+    public Enemy(int width, int height, int posX, int posY) {
+        this.width = width;
+        this.height = height;
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+}
--- src/com/company/Pannau.java
+++ src/com/company/Pannau.java
@@ -2,22 +2,47 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Oussama on 01/10/2016.
  */
 public class Pannau extends JPanel {
     int x = 290, y = 69;
+    ArrayList[] table = new ArrayList[16];
+    Random r = new Random();
+
 
     public void paintComponent(Graphics g) {
         g.setColor(Color.black);
         g.fillRect(0, 0, getWidth(), getHeight());
         g.setColor(Color.blue);
         g.fillOval(x, y, 30, 30);
+        stagesCreator(g);
+    }
 
+    public void stagesCreator(Graphics g){
         for (int i = 2; i < 18; i++) {
             g.setColor(Color.white);
             g.drawLine(0, i * 50, getWidth(), i * 50);
+            table[i - 2] = new ArrayList<Enemy>();
+            enemyCreator(table[i - 2], i);
+            g.setColor(Color.white);
+            for (int j = 0; j < table[i - 2].size(); j++) {
+                Enemy e = (Enemy) table[i - 2].get(j);
+                g.fillRect(e.getPosX(), e.getPosY(), e.getWidth(), e.getHeight());
+            }
         }
     }
-}
+
+    public void enemyCreator(ArrayList<Enemy> arrayList, int level) {
+        int lenght = r.nextInt(3) + 3;
+        for (int i = 0; i < getWidth() / lenght; i++) {
+            Enemy e = new Enemy(30, 50, i * (getWidth() / lenght), level * 50);
+            arrayList.add(e);
+            }
+        }
+
+
+    }
